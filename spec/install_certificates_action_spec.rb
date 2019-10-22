9e3b1f60a3cde912029672c4dff588d8b3422350
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2019-10-22 09:20:37 +0200

    Add tests

--- spec/install_certificates_action_spec.rb
+++ spec/install_certificates_action_spec.rb
@@ -3,7 +3,7 @@ describe Fastlane::Actions::InstallCertificatesAction do
     it 'prints a message' do
       expect(Fastlane::UI).to receive(:message).with("The install_certificates plugin is working!")
 
-      Fastlane::Actions::InstallCertificatesAction.run(nil)
+      Fastlane::Actions::InstallCertificatesAction.run(passphrase: "")
     end
   end
 end
